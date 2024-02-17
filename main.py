5a42ee13cfeb075f591952fe74042e2355d30b9a
SonuKamble <ddkamble2141@gmail.com>
2024-02-17 16:23:46 +0530

    post command

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app= FastAPI()
+
+class Data(BaseModel):
+    name:str
+    age:int
 
 @app.get("/")
 def read_root():
@@ -11,3 +16,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int,q: Union[str,None]=None):
     return {"item_id":item_id,"q":q}
+
+@app.post("/items")
+def pst_item(data: Data):
+    return(data.name,data.age)
